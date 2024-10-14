79d8266507f51b54d615380bffbea8e4c3ddb09b
claudia13-prog <cappellaclaudia13@gmail.com>
2024-10-14 16:35:03 +0200

    added "for" cicle

---
+++ main.js
@@ -0,0 +1,11 @@
+// 1) Inizializzo una variabile index e le assegno il valore iniziale di 1.
+
+// 2) Stabilisco la condizione da verificare: se la variabile index è minore o uguale a 100, la condinzione è vera.
+
+// 3) Imposto l'incremento del valore di index.
+
+for(let i = 1; i <= 100; i++) { 
+
+
+}
+
