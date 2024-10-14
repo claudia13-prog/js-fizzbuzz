af9ff9e8b49948475917e2ff4adc672bee85f9fd
claudia13-prog <cappellaclaudia13@gmail.com>
2024-10-14 16:46:17 +0200

    added the if conditionals

--- main.js
+++ main.js
@@ -5,7 +5,25 @@
 // 3) Imposto l'incremento del valore di index.
 
 for(let i = 1; i <= 100; i++) { 
+   
+/* 4) Aggiungo altre tre condizioni:
+- se index è multiplo di 3 stampo "Fizz";
+- se index è multiplo di 5 stampo "Buzz";
+- se index è multiplo di 3 e di 5 stampo "FizzBuzz".
+*/
 
+if(i % 3 === 0 && i % 5 === 0){
+    console.log('FizzBuzz');
 
+} else if(i % 3 === 0){
+    console.log('Fizz');
+
+} else if(i % 5 === 0){
+    console.log('Buzz');
 }
 
+ else{
+    console.log(i);
+}
+
+}
